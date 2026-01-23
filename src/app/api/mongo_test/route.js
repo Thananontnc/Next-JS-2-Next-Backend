de12341ea16379dd32a8fc2111523c3dea285715
Thananon Chounudom <thananonchounudom@Thananons-MacBook-Pro.local>
2026-01-23 16:18:19 +0700

    Add mongo_test API with dynamic routes

---
+++ src/app/api/mongo_test/route.js
@@ -0,0 +1,31 @@
+import corsHeaders from "@/lib/cors";
+import { getClientPromise } from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export async function OPTIONS(req) {
+    return new Response(null, {
+        status: 200,
+        headers: corsHeaders,
+    });
+}
+
+export async function GET() {
+    try {
+        const client = await getClientPromise();
+        const db = client.db("sample_mflix");
+        const result = await db.collection("movies").find({}).limit(20).toArray();
+        console.log("==> result", result);
+        return NextResponse.json(result, {
+            headers: corsHeaders
+        });
+    } catch (exception) {
+        console.log("exception", exception.toString());
+        const errorMsg = exception.toString();
+        return NextResponse.json({
+            message: errorMsg
+        }, {
+            status: 400,
+            headers: corsHeaders
+        })
+    }
+}
